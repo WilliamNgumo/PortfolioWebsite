8a03776b99875fc559444c0f2d379348a0d0a974
WilliamNgumo <ngumpwilliam@gmail.com>
2025-02-15 15:33:10 -0600

    almost done with portfolio webiste. Just checking for github connection

---
+++ index.js
@@ -0,0 +1,6 @@
+let menu= document.querySelector('.header .fa-bars-staggered');
+let navbar= document.querySelector('.header .navbar');
+
+menu.onclick=()=>{
+    navbar.classList.toggle('active');
+}
